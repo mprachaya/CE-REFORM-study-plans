2aac7d9a8443b96d52f7179f0882506d42eb3ee3
= <=>
2023-11-10 14:36:01 +0700

    Fix useSubmit remove second then

--- src/hooks/useSubmit.js
+++ src/hooks/useSubmit.js
@@ -5,14 +5,14 @@ export default function useSubmit(url, state, closemodal, refetch) {
   if (state) {
     axios
       .post(url, state)
-      .then(response => response.data)
-      .then(res => {
+      .then(response => {
         console.log(res.data)
         if (res.data.status === 201) {
           closemodal()
           refetch()
         }
       })
+
       .catch(err => {
         console.log('An error occurred. Awkward.. : ', err)
         alert('Status: ' + err.response.data.status + ' ' + err.response.data.message)
