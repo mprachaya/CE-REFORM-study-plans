757de72204eaa81397597691abea454f7561e894
= <=>
2023-11-10 14:31:39 +0700

    Fix submit

--- src/hooks/useSubmit.js
+++ src/hooks/useSubmit.js
@@ -8,14 +8,14 @@ export default function useSubmit(url, state, closemodal, refetch) {
       .then(response => response.data)
       .then(res => {
         console.log(res.data)
+        if (res.data.status === 201) {
+          closemodal()
+          refetch()
+        }
       })
       .catch(err => {
         console.log('An error occurred. Awkward.. : ', err)
         alert('Status: ' + err.response.data.status + ' ' + err.response.data.message)
-      })
-      .finally(() => {
-        closemodal()
-        refetch()
       })
   } else {
     return false
