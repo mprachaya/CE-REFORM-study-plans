66f9445d63e1696b993220b09a203e827f46e1b6
= <=>
2023-11-24 01:21:13 +0700

    subject structures (beta) done * test again after fix api

--- src/pages/pages/masterdata/subjectstructures/index.js
+++ src/pages/pages/masterdata/subjectstructures/index.js
@@ -10,10 +10,12 @@ import {
   Dialog,
   DialogTitle,
   DialogContent,
+  DialogActions,
   TextField,
   Chip,
   Card
 } from '@mui/material'
+import axios from 'axios'
 import { mdiPen, mdiTrashCan } from '@mdi/js'
 import Icon from '@mdi/react'
 import { useFetch } from 'src/hooks'
@@ -21,11 +23,29 @@ import { url } from 'src/configs/urlConfig'
 import { Btn, DataGridTable, Selection } from 'src/components'
 
 function subjectstructures() {
-  const [curriculumSelected, setCurriculumSelected] = useState(0)
-
   const URL_GET_CURRICULUM = `${url.BASE_URL}/curriculums/`
   const URL_GET_SUBJECT_STRUCTURES = `${url.BASE_URL}/subject-structures/`
   const URL_GET_CATEGORIES = `${url.BASE_URL}/subject-categories/`
+  const URL_GET_TYPES = `${url.BASE_URL}/subject-types/`
+  const URL_GET_GROUPS = `${url.BASE_URL}/subject-groups/`
+  const URL_GET_SUBJECTS_BY_CURRICURUM = `${url.BASE_URL}/subjects-by-curriculum/`
+
+  const [curriculumSelected, setCurriculumSelected] = useState(0)
+  const [categorySelected, setCategorySelected] = useState(0)
+  const [typeSelected, setTypeSelected] = useState(0)
+  const [groupSelected, setGroupSelected] = useState(0)
+  const [subject, setSubject] = useState([])
+  const [open, setOpen] = useState(false)
+  // 0 -> insert , 1 -> edit
+  const [formType, setFormType] = useState(0)
+  const initialState = {
+    subject_category_id: '',
+    subject_type_id: '',
+    subject_group_id: '',
+    subject_id: ''
+  }
+  const [state, setState] = useState(initialState)
+  const [subjectStructureId, setSubjectStructureId] = useState(0)
 
   const {
     error: CurriculumError,
@@ -36,12 +56,34 @@ function subjectstructures() {
   } = useFetch(URL_GET_CURRICULUM)
 
   const {
+    error: SubjectsError,
+    data: Subjects,
+    setData: setSubjects,
+    loading: SubjectsLoading,
+    reFetch: reFetchSubjects
+  } = useFetch(URL_GET_SUBJECTS_BY_CURRICURUM + curriculumSelected)
+
+  const {
     error: CategoriesError,
     data: Categories,
     setData: setCategories,
     loading: CategoriesLoading,
     reFetch: reFetchCategories
   } = useFetch(URL_GET_CATEGORIES)
+  const {
+    error: TypesError,
+    data: Types,
+    setData: setTypes,
+    loading: TypesLoading,
+    reFetch: reFetchTypes
+  } = useFetch(URL_GET_TYPES)
+  const {
+    error: GroupsError,
+    data: Groups,
+    setData: setGroups,
+    loading: GroupsLoading,
+    reFetch: reFetchGroups
+  } = useFetch(URL_GET_GROUPS)
 
   const {
     error: SubjectStructuresError,
@@ -50,6 +92,96 @@ function subjectstructures() {
     loading: SubjectStructuresLoading,
     reFetch: reFetchSubjectStructures
   } = useFetch(URL_GET_SUBJECT_STRUCTURES + curriculumSelected)
+
+  const handleOpenForm = (type, structureId, subject, sjCategory, sjType, sjGroup) => {
+    if (type === 0) {
+      // insert
+      setFormType(0)
+      setOpen(true)
+      setSubject(null)
+      setCategorySelected(null)
+      setGroupSelected(null)
+      setTypeSelected(null)
+    } else {
+      // edit
+      if (subject) {
+        setState({
+          subject_category_id: sjCategory?.subject_category_id,
+          subject_type_id: sjType?.subject_type_id,
+          subject_group_id: sjGroup?.subject_group_id,
+          subject_id: subject?.subject_id
+        })
+        setFormType(1)
+        setOpen(true)
+        setSubject(subject)
+        setCategorySelected(sjCategory)
+        setTypeSelected(sjType)
+        setGroupSelected(sjGroup)
+      }
+      if (structureId) setSubjectStructureId(structureId)
+    }
+  }
+
+  const handleSubmit = () => {
+    if (state.subject_id !== '' && state.subject_category_id !== '') {
+      axios
+        .get(URL_GET_SUBJECT_STRUCTURES + curriculumSelected)
+        .then(res => {
+          if (res.data) {
+            const duplicate = Object.values(res.data.data)?.find(d => d.subject.subject_id === state.subject_id)
+            if (duplicate) {
+              alert('this subject is already exist in curriculum!')
+            } else {
+              axios
+                .post(URL_GET_SUBJECT_STRUCTURES, state)
+                .then(res => {
+                  if (res.data) {
+                    setState(initialState)
+                    setOpen(false)
+                    reFetchSubjectStructures()
+                  }
+                })
+                .catch(err => console.log('error from insert new subject structure: ', err))
+            }
+          }
+        })
+        .catch(err => console.log('error from check duplicate:', err))
+    } else {
+      alert('Please Select Subject and Category')
+    }
+  }
+  const handleUpdate = () => {
+    if (state.subject_id !== '' && state.subject_category_id !== '') {
+      axios
+        .put(URL_GET_SUBJECT_STRUCTURES + subjectStructureId, state)
+        .then(res => {
+          if (res.data) {
+            setState(initialState)
+            setOpen(false)
+            reFetchSubjectStructures()
+          }
+        })
+        .catch(err => console.log('error from update subject structure: ', err))
+    } else {
+      alert('Please Select Subject and Category')
+    }
+  }
+  const handleDelete = id => {
+    let result = window.confirm('Confirm to Delete?')
+    if (result) {
+      axios
+        .delete(URL_GET_SUBJECT_STRUCTURES + id)
+        .then(res => {
+          if (res.data) {
+            setState(initialState)
+            setOpen(false)
+            reFetchSubjectStructures()
+            console.log(res.data)
+          }
+        })
+        .catch(err => console.log('error from delete subject structure', err))
+    }
+  }
 
   useEffect(() => {
     if (Curriculums.length > 0) {
@@ -62,7 +194,13 @@ function subjectstructures() {
     }
   }, [Curriculums])
 
-  console.log(Categories)
+  useEffect(() => {
+    if (state) {
+      console.log(state)
+    }
+  }, [state])
+
+  // console.log(Categories)
 
   const columns = [
     {
@@ -97,8 +235,7 @@ function subjectstructures() {
     },
 
     {
-      field: 'fn',
-
+      field: 'E/D',
       width: 300,
       renderCell: params => (
         <Grid container spacing={2}>
@@ -107,7 +244,16 @@ function subjectstructures() {
               size='small'
               color='secondary'
               variant='outlined'
-              //  onClick={() => handleClickOpenEdit(params.row)}
+              onClick={() =>
+                handleOpenForm(
+                  1,
+                  params?.row?.subject_structure_id,
+                  params?.row?.subject,
+                  params.row?.subjectCategory,
+                  params.row?.subjectType,
+                  params.row?.subjectGroup
+                )
+              }
             >
               <Icon path={mdiPen} size={1} />
             </Button>
@@ -117,7 +263,7 @@ function subjectstructures() {
               size='small'
               color='error'
               variant='outlined'
-              // onClick={() => handleClickOpenDetails(params.row)}
+              onClick={() => handleDelete(params?.row?.subject_structure_id)}
             >
               <Icon path={mdiTrashCan} size={1} />
             </Button>
@@ -150,7 +296,7 @@ function subjectstructures() {
           </Box>
         </Grid>
         <Grid item xs={12} md={4} lg={3.5}>
-          <Btn width={'100%'} handleclick={() => void 0} label={'+ Add New'} />
+          <Btn width={'100%'} handleclick={() => handleOpenForm(0)} label={'+ Add New'} />
         </Grid>
         <Grid item xs={12}>
           <DataGridTable
@@ -161,6 +307,144 @@ function subjectstructures() {
             isLoading={CurriculumLoading === null ? true : CurriculumLoading}
           />
         </Grid>
+        {/* form dialog */}
+        <Dialog
+          open={open}
+          onClose={() => {
+            setState(initialState)
+            setOpen(false)
+          }}
+          fullWidth
+          maxWidth={'md'}
+        >
+          <DialogTitle>Form Subject Structures</DialogTitle>
+          <DialogContent sx={{ minHeight: 450 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sx={{ my: 2 }}>
+                {formType === 0 ? (
+                  <Autocomplete
+                    size='small'
+                    disablePortal
+                    fullWidth
+                    freeSolo
+                    options={Subjects || []}
+                    getOptionLabel={option => option?.subject_code + ' ' + option?.subject_name_th || ''}
+                    renderInput={params => <TextField {...params} label='Subject' />}
+                    onChange={(e, value) => {
+                      if (value !== null) {
+                        setSubject(value)
+                        setState(pre => ({ ...pre, subject_id: value.subject_id }))
+                      } else {
+                        setSubject([])
+                        setState(pre => ({ ...pre, subject_id: '' }))
+                      }
+                    }}
+                  />
+                ) : (
+                  <Autocomplete
+                    disabled={true}
+                    size='small'
+                    disablePortal
+                    fullWidth
+                    freeSolo
+                    value={subject}
+                    options={Subjects || []}
+                    getOptionLabel={option => option?.subject_code + ' ' + option?.subject_name_th || ''}
+                    renderInput={params => <TextField {...params} label='Subject' />}
+                  />
+                )}
+              </Grid>
+              <Grid item xs={12} sx={{ my: 2 }}>
+                <Autocomplete
+                  value={categorySelected}
+                  size='small'
+                  disablePortal
+                  fullWidth
+                  freeSolo
+                  options={Categories || []}
+                  getOptionLabel={option => option?.subject_category_name || ''}
+                  renderInput={params => <TextField {...params} label='Subject Category' />}
+                  onChange={(e, value) => {
+                    if (value !== null) {
+                      setCategorySelected(value)
+                      setState(pre => ({ ...pre, subject_category_id: value.subject_category_id }))
+                    } else {
+                      setCategorySelected([])
+                      setState(pre => ({ ...pre, subject_category_id: '' }))
+                    }
+                  }}
+                />
+              </Grid>
+              <Grid item xs={12} sx={{ my: 2 }}>
+                <Autocomplete
+                  value={typeSelected}
+                  size='small'
+                  disablePortal
+                  fullWidth
+                  freeSolo
+                  // options={Jobs?.filter(sj => sj.subject_id !== subject.subject_id)}
+                  options={Types || []}
+                  getOptionLabel={option => option?.subject_type_name || ''}
+                  renderInput={params => <TextField {...params} label='Subject Type' />}
+                  onChange={(e, value) => {
+                    if (value !== null) {
+                      setTypeSelected(value)
+                      setState(pre => ({ ...pre, subject_type_id: value.subject_type_id }))
+                    } else {
+                      setTypeSelected([])
+                      setState(pre => ({ ...pre, subject_type_id: '' }))
+                    }
+                  }}
+                />
+              </Grid>
+              <Grid item xs={12} sx={{ my: 2 }}>
+                <Autocomplete
+                  value={groupSelected}
+                  size='small'
+                  disablePortal
+                  fullWidth
+                  freeSolo
+                  options={
+                    // only uniqe subject group name
+                    Groups.filter((obj, index, arr) => {
+                      const firstIndex = arr.findIndex(item => item.subject_group_name === obj.subject_group_name)
+                      return index === firstIndex
+                    }) || []
+                  }
+                  getOptionLabel={option => option?.subject_group_name || ''}
+                  renderInput={params => <TextField {...params} label='Subject Group' />}
+                  onChange={(e, value) => {
+                    if (value !== null) {
+                      setGroupSelected(value)
+                      setState(pre => ({ ...pre, subject_group_id: value.subject_group_id }))
+                    } else {
+                      setGroupSelected([])
+                      setState(pre => ({ ...pre, subject_group_id: '' }))
+                    }
+                  }}
+                />
+              </Grid>
+            </Grid>
+          </DialogContent>
+          <DialogActions>
+            <Button
+              onClick={() => {
+                formType === 0 ? handleSubmit() : handleUpdate()
+              }}
+              variant='contained'
+            >
+              {formType === 0 ? 'Submit' : 'Update'}
+            </Button>
+            <Button
+              onClick={() => {
+                setState(initialState)
+                setOpen(false)
+              }}
+            >
+              Close
+            </Button>
+          </DialogActions>
+        </Dialog>
       </Grid>
     </>
   )
